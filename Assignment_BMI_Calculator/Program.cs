83bf82b7a990b2ae1528a0a1336bd98d63a64298
Jasleen <Jasleen@LAPTOP-IREO6BKM>
2017-08-13 14:56:53 -0400

    SplashForm Created

--- Assignment_BMI_Calculator/Program.cs
+++ Assignment_BMI_Calculator/Program.cs
@@ -4,6 +4,11 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+/*Name: Jasleen
+ Student Number: 300929376
+ Description: Project created on 13 august 
+ version :0.2 - started with splash form*/
+
 namespace Assignment_BMI_Calculator
 {
     static class Program
